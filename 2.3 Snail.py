862ec8a8eff7714a8b704a345f114cb83c20bd2d
KonstantinLitvin <konstantinlitvin89@gmail.com>
2016-07-21 17:05:25 +0300

    Start solving 2.3 Snail

---
+++ 2.3 Snail.py
@@ -0,0 +1,14 @@
+__author__ = 'KonstantinLitvin'
+
+H = int(input())
+A = int(input())
+B = int(input())
+
+fullDay = A - B
+
+x = H // fullDay
+
+#if H == x:
+#    print(x - B)
+#else:
+#    print(x - B + 1)
